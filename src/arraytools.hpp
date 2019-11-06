f55cce3c1212556cb3cd2d33608906a24128716a
Drew Schmidt <wrathematics@gmail.com>
2019-11-05 19:40:53 -0500

    add license boilerplate to source files

--- src/arraytools.hpp
+++ src/arraytools.hpp
@@ -1,3 +1,7 @@
+// This file is part of spvec which is released under the Boost Software
+// License, Version 1.0. See accompanying file LICENSE or copy at
+// https://www.boost.org/LICENSE_1_0.txt
+
 #ifndef SPVEC_ARRAYTOOLS_H
 #define SPVEC_ARRAYTOOLS_H
 
