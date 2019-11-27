d93dc3c4e19e7f173294def67bc7491b58eb65d1
Drew Schmidt <wrathematics@gmail.com>
2019-11-26 21:12:20 -0500

    add fltcmp bits

--- src/arraytools.hpp
+++ src/arraytools.hpp
@@ -7,9 +7,16 @@
 #pragma once
 
 
+#include <algorithm>
+#include <cfloat>
+#include <cmath>
 #include <cstdlib>
 #include <cstring>
 #include <stdexcept>
+
+#ifdef __CUDACC__
+#include <cublas.h>
+#endif
 
 
 namespace arraytools
@@ -177,6 +184,142 @@ namespace arraytools
   
   
   
+  namespace fltcmp
+  {
+    namespace
+    {
+      // eps ~ sqrt(precision)
+    #ifdef __CUDACC__
+      const __half eps_hlf = 1e-2; // machine epsilon 0x1p-11 ~ 2.2e-2
+    #endif
+      const float eps_flt = 1e-4;
+      const double eps_dbl = 1e-8;
+      
+      // smallest representible number with 1 as the leading bit of the significand
+    #ifdef __CUDACC__
+      const __half min_normal_hlf = 0x1p-14; // (2^(5-1)-1)-1 = 14
+    #endif
+      const float min_normal_flt = 0x1p-126;
+      const double min_normal_dbl = 0x1p-1022;
+      
+    #ifdef __CUDACC__
+      const __half max_hlf = 1>>15;
+    #endif
+      const float max_flt = FLT_MAX;
+      const double max_dbl = DBL_MAX;
+      
+      
+      
+    #ifdef __CUDACC__
+      static inline __half fabsh(const __half x)
+      {
+        return __float2half(fabsf(__half2float(x)));
+      }
+    #endif
+      
+      
+      
+      static inline bool subnormal(const float x)
+      {
+        return (x < min_normal_flt);
+      }
+      
+      static inline bool subnormal(const double x)
+      {
+        return (x < min_normal_dbl);
+      }
+      
+      
+      
+      template <typename T>
+      static inline bool abseq(const T x, const T y)
+      {
+        float fx = (float) x;
+        float fy = (float) y;
+        return fabsf(fx - fy) < (eps_flt * min_normal_flt);
+      }
+      
+      static inline bool abseq(const float x, const float y)
+      {
+        return fabsf(x - y) < (eps_flt * min_normal_flt);
+      }
+      
+      static inline bool abseq(const double x, const double y)
+      {
+        return fabs(x - y) < (eps_dbl * min_normal_dbl);
+      }
+      
+      
+      
+      template <typename T>
+      static inline bool releq(const T x, const T y)
+      {
+        float fx = (float) x;
+        float fy = (float) y;
+        return fabsf(fx - fy) / std::min(fabsf(fx)+fabsf(fy), max_flt) < eps_flt;
+      }
+      
+      static inline bool releq(const float x, const float y)
+      {
+        return fabsf(x - y) / std::min(fabsf(x)+fabsf(y), max_flt) < eps_flt;
+      }
+      
+      static inline bool releq(const double x, const double y)
+      {
+        return fabs(x - y) / std::min(fabs(x)+fabs(y), max_dbl) < eps_dbl;
+      }
+    }
+    
+    
+    
+    // modified from https://floating-point-gui.de/errors/comparison/
+    template <typename REAL>
+    static inline bool eq(const REAL x, const REAL y)
+    {
+      if (x == y)
+        return true;
+      else if (x == 0.f || y == 0.f || subnormal(fabs(x)+fabs(y)))
+        return abseq(x, y);
+      else
+        return releq(x, y);
+    }
+    
+    
+    
+    static inline bool eq(const int x, const int y)
+    {
+      return (x == y);
+    }
+    
+    
+    
+    template <typename REAL>
+    static inline bool eq(const REAL x, const int y)
+    {
+      return eq(x, (REAL) y);
+    }
+    
+    template <typename REAL>
+    static inline bool eq(const int x, const REAL y)
+    {
+      return eq((REAL) x, y);
+    }
+    
+    
+    
+    static inline bool eq(const float x, double y)
+    {
+      return eq(x, (float) y);
+    }
+    
+    static inline bool eq(const double x, const float y)
+    {
+      return eq((float) x, y);
+    }
+  }
+  
+  
+  
   /** 
    * Do the two arrays contain the same elements (up to a cast)? Not suitable
    * for floats.
@@ -193,7 +336,7 @@ namespace arraytools
     #pragma omp simd reduction(min:ret)
     for (size_t i=0; i<len; i++)
     {
-      if (a[i] != (TA)b[i])
+      if (!fltcmp::eq(a[i], b[i]))
         ret = i;
     }
     
