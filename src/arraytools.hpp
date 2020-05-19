4ada8320d542178b667a51b84d50b63cd213671c
Drew Schmidt <wrathematics@gmail.com>
2020-05-18 21:49:07 -0400

    tweak header guard

--- src/arraytools.hpp
+++ src/arraytools.hpp
@@ -2,8 +2,8 @@
 // License, Version 1.0. See accompanying file LICENSE or copy at
 // https://www.boost.org/LICENSE_1_0.txt
 
-#ifndef SPVEC_ARRAYTOOLS_H
-#define SPVEC_ARRAYTOOLS_H
+#ifndef ARRAYTOOLS_HPP
+#define ARRAYTOOLS_HPP
 #pragma once
 
 
